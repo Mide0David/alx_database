213ada5168a58f3db0c2eda999344aa279082898
david Babalola <midebabalola7@gmail.com>
2023-10-19 09:22:14 +0000

    fixed issue

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,7 @@
 -- Use the 'hbtn_0d_usa' database
 -- Select all cities of California
 
-USE hbtn_0d_usa;
+USE hbtn_test_db_8;
 
 SELECT * FROM cities
 WHERE state_id = (SELECT id FROM states WHERE name = 'California')
